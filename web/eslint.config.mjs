a5091c7e1e6c9f1dcfd2ae4b7371045f7ccd8bec
Alerrando <alerrando2@gmail.com>
2024-12-26 01:24:35 -0300

    feat: add eslint

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,16 +1,53 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier";
+import pluginJsxA11y from "eslint-plugin-jsx-a11y";
+import pluginSimpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+      "jsx-a11y": pluginJsxA11y,
+      "simple-import-sort": pluginSimpleImportSort,
+    },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 120,
+          tabWidth: 2,
+          singleQuote: false,
+          trailingComma: "all",
+          arrowParens: "always",
+          semi: true,
+          endOfLine: "auto",
+        },
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "jsx-a11y/alt-text": [
+        "warn",
+        {
+          elements: ["img"],
+          img: ["Image"],
+        },
+      ],
+      "jsx-a11y/aria-props": "warn",
+      "jsx-a11y/aria-proptypes": "warn",
+      "jsx-a11y/aria-unsupported-elements": "warn",
+      "jsx-a11y/role-has-required-aria-props": "warn",
+      "jsx-a11y/role-supports-aria-props": "warn",
+    },
+  },
 ];
-
-export default eslintConfig;

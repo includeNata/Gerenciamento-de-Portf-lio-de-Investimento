77c76eb6524d74d557bec53fff15f168048d9731
ThalesJ2 <thalesjorge36@gmail.com>
2024-12-29 13:03:35 -0300

    feat: create entities user and role

---
+++ server/src/main/java/projectn/com/server/entities/User.java
@@ -0,0 +1,74 @@
+package projectn.com.server.entities;
+
+import jakarta.persistence.*;
+
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "tb_users_roles" ,
+        joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+       inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
+    private Set<Role> roles;
+    public User() {
+    }
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}

7506d83ae061f0307f100013183e64ef6dfc9df1
Alerrando <alerrando2@gmail.com>
2025-01-21 02:20:35 -0300

    feat: add section of result in CalcCompoundInterest

--- web/src/app/investment-planner/CalcCompoundInterest/CalcCompoundInterest.tsx
+++ web/src/app/investment-planner/CalcCompoundInterest/CalcCompoundInterest.tsx
@@ -1,10 +1,16 @@
+"use client";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 
 import { Button } from "@/components/ui/button";
+import { Command, CommandGroup, CommandItem, CommandList } from "@/components/ui/command";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 
 const schemaData = Yup.object({
   valueInitial: Yup.number().required("Campo obrigatório"),
@@ -12,6 +18,19 @@ const schemaData = Yup.object({
   interestRate: Yup.number().required("Campo obrigatório"),
   months: Yup.number().required("Campo obrigatório"),
 });
+
+type SchemaDataType = Yup.InferType<typeof schemaData>;
+
+const frameworks = [
+  {
+    value: "month",
+    label: "Mês (s)",
+  },
+  {
+    value: "year",
+    label: "Ano (s)",
+  },
+];
 
 export default function CalcCompoundInterest() {
   const {
@@ -28,9 +47,32 @@ export default function CalcCompoundInterest() {
       months: 12,
     },
   });
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("month");
+  const [valuesInterest, setValuesInterest] = useState<number[]>([]);
 
-  function submit(e: typeof schemaData) {
-    console.log(e);
+  function submit(e: SchemaDataType) {
+    const { valueInitial, valueMonthly, interestRate, months } = e;
+
+    let totalAmount = 0;
+
+    const monthRate = interestRate / 100 / 12;
+    const monthsLoop = value === "month" ? months : months * 12;
+
+    let amountFromInitial = 0;
+    let amountFromMonthlyContributions = amountFromInitial;
+    let calcRateMonthAccumulate = 0;
+    console.log(monthsLoop);
+
+    for (let i = 1; i < monthsLoop + 1; i++) {
+      amountFromInitial = valueMonthly + amountFromInitial;
+      const calcRateMonth = amountFromMonthlyContributions * monthRate;
+      calcRateMonthAccumulate += calcRateMonth;
+      amountFromMonthlyContributions = amountFromInitial + calcRateMonthAccumulate;
+    }
+
+    totalAmount = amountFromMonthlyContributions + valueInitial;
+    setValuesInterest([totalAmount, calcRateMonthAccumulate, valueMonthly * monthsLoop + valueInitial]);
   }
 
   return (
@@ -110,6 +152,41 @@ export default function CalcCompoundInterest() {
               placeholder="12"
               className="h-full w-full rounded-r-lg border-none bg-transparent px-4 py-2 text-lg text-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-500 focus-visible:ring-0 dark:text-white"
             />
+
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-24 justify-between dark:bg-gray-600 dark:text-gray-100"
+                >
+                  {value ? frameworks.find((framework) => framework.value === value)?.label : setValue("month")}
+                  <ChevronsUpDown className="opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0">
+                <Command>
+                  <CommandList>
+                    <CommandGroup>
+                      {frameworks.map((framework) => (
+                        <CommandItem
+                          key={framework.value}
+                          value={framework.value}
+                          onSelect={(currentValue) => {
+                            setValue(currentValue === value ? "" : currentValue);
+                            setOpen(false);
+                          }}
+                        >
+                          {framework.label}
+                          <Check className={cn("ml-auto", value === framework.value ? "opacity-100" : "opacity-0")} />
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
           </div>
           {errors.months && <span className="text-red-500">{errors.months.message}</span>}
         </div>
@@ -126,6 +203,35 @@ export default function CalcCompoundInterest() {
           Calcular
         </Button>
       </div>
+
+      {valuesInterest.length > 0 && (
+        <div className="mt-8 grid w-full grid-cols-1 justify-center gap-6 sm:grid-cols-3">
+          <div className="flex w-full flex-col items-center justify-center rounded-lg bg-white p-6 shadow-lg dark:bg-gray-800 sm:w-auto">
+            <span className="text-lg font-medium text-gray-700 dark:text-white">Montante Total</span>
+            <span className="mt-2 text-2xl font-semibold text-purple-600 dark:text-purple-400">
+              {valuesInterest[0].toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </span>
+          </div>
+          <div className="flex w-full flex-col items-center justify-center rounded-lg bg-white p-6 shadow-lg dark:bg-gray-800 sm:w-auto">
+            <span className="text-lg font-medium text-gray-700 dark:text-white">Juros Acumulados</span>
+            <span className="mt-2 text-2xl font-semibold text-purple-600 dark:text-purple-400">
+              {valuesInterest[1].toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
+            </span>
+          </div>
+          <div className="flex w-full flex-col items-center justify-center rounded-lg bg-white p-6 shadow-lg dark:bg-gray-800 sm:w-auto">
+            <span className="text-lg font-medium text-gray-700 dark:text-white">Contribuições Mensais Totais</span>
+            <span className="mt-2 text-2xl font-semibold text-purple-600 dark:text-purple-400">
+              {valuesInterest[2].toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </span>
+          </div>
+        </div>
+      )}
     </form>
   );
 }

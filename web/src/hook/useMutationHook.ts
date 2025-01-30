5f122344b5792d15132de33e95b853b8a2113205
Alerrando <alerrando2@gmail.com>
2025-01-30 01:24:53 -0300

    remove: removing gcTime in useMutationHook and add in option of useMutationHook

--- web/src/hook/useMutationHook.ts
+++ web/src/hook/useMutationHook.ts
@@ -5,13 +5,11 @@ import { MutationModel } from "../models/MutationModel";
 export const useMutationHook = <TData = unknown, TError = unknown, TVariables = void, TContext = unknown>({
   mutationFn,
   onSuccess,
-  gcTime,
   options,
 }: MutationModel<TData, TError, TVariables, TContext>): UseMutationResult<TData, TError, TVariables, TContext> => {
   return useMutation({
     mutationFn,
     onSuccess,
-    gcTime,
     ...options,
   });
 };
--- web/src/models/MutationModel.ts
+++ web/src/models/MutationModel.ts
@@ -3,6 +3,5 @@ import { MutationFunction, UseMutationOptions } from "@tanstack/react-query";
 export interface MutationModel<TData = unknown, TError = unknown, TVariables = void, TContext = unknown> {
   mutationFn: MutationFunction<TData, TVariables>;
   onSuccess?: (data: TData, variables: TVariables, context: TContext) => void | Promise<void>;
-  gcTime?: number;
   options?: UseMutationOptions<TData, TError, TVariables, TContext>;
 }

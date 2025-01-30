e973a3b569446c4519a6b0ae941e5b0e972b5e4a
Alerrando <alerrando2@gmail.com>
2025-01-30 01:56:01 -0300

    remove: removing useMutationHook and MutationModel

--- web/src/models/MutationModel.ts
+++
@@ -1,7 +0,0 @@
-import { MutationFunction, UseMutationOptions } from "@tanstack/react-query";
-
-export interface MutationModel<TData = unknown, TError = unknown, TVariables = void, TContext = unknown> {
-  mutationFn: MutationFunction<TData, TVariables>;
-  onSuccess?: (data: TData, variables: TVariables, context: TContext) => void | Promise<void>;
-  options?: UseMutationOptions<TData, TError, TVariables, TContext>;
-}

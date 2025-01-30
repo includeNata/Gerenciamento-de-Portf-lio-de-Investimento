a61cb80d9253e672a52aa0a681cd2bedff2517c9
Alerrando <alerrando2@gmail.com>
2025-01-30 00:10:20 -0300

    fix: fixing eslint error

--- web/src/components/RankingCard/RankingCardFB/RankingCardFiis/RankingCardFiis.tsx
+++ web/src/components/RankingCard/RankingCardFB/RankingCardFiis/RankingCardFiis.tsx
@@ -53,7 +53,7 @@ export default function RankingCardFiis({ title, data, onViewAll, styleRankingCa
                     <div className="flex w-fit flex-col items-end">
                       <span>{item.volume}</span>
                       <span
-                        className={`text-[10px] ${item.volumePercentage.split("-")[0] > 0 ? "text-green-500" : "text-red-500"}`}
+                        className={`text-[10px] ${parseFloat(item.volumePercentage.split("-")[0]) > 0 ? "text-green-500" : "text-red-500"}`}
                       >
                         {item.volumePercentage}
                       </span>
